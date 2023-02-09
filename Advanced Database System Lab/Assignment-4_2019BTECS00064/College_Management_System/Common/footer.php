a4636b243c1d0c5715acdd6a49213e9afc2a161d
Prajakta Sandeep Chavan <110226806+PrajaktaC-04@users.noreply.github.com>
2023-02-09 23:11:26 +0530

    Add files via upload

---
+++ Advanced Database System Lab/Assignment-4_2019BTECS00064/College_Management_System/Common/footer.php
@@ -0,0 +1,45 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Walchand College of Engineering</h3>
+            <p>This Project has been made by Kunal Kadam.
+              The prupose of this project is Learning Computer Languages.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About WCE</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Tech. Civil Engineering</a></li>
+              <li><a href="">B.Tech. Electrical Engineering</a></li>
+              <li><a href="">B.Tech. Mechanical Engineering</a></li>
+              <li><a href="">B.Tech. Electronics</a></li>
+              <li><a href="">B.Tech. Computer Science</a></li>
+              <li><a href="">B.Tech. Information Technology</a></li>
+              <!-- <li><a href="">Bachelors of Business Administration</a></li> -->
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Walchand College of Engineering,
+                  Miraj Rd, Vishrambag, Sangli,Maharashtra – India</p>
+              <p>Telephones: (+91) 233-2300383</p>
+              <p>E-mail: registrar@walchandsangli.ac.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by Kunal Kadam.2022</p>
+</footer>
---
+++ Advanced Database System Lab/Assignment-4_2019BTECS00064/College_Management_System/academics.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academics</title>
+</head>
+<body>
+	<?php include('common/header.php') ?>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Civil Engineering</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">HOD's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							 It gives me a sense of happiness and pride to introduce my department’s profile in the year 2021-2022. With a strong commitment we keep marching towards vision and missions of our department and institute as a whole. This is yet another year which showcased the rich tradition of department on the fronts of academics, research, consultancy, industry-institute interactions and student’s empowerment. Industry-Institute interaction is key focus area which helps us enrich our academic programmes and keep them up to date and relevant. Industry professionals were invited to guide and advice students on many occasions. Core UG and two PG programmes are strengthened by eminent industry experts through various MoU’s and up to date curriculum that matches the expectations of industry to a great extent. Recently the third year UG and second year PG programme curriculum were updated as per BoS suggestions which are in tune with the modern AICTE guidelines. Interaction with industry has made the UG and PG streams highly relevant in civil engineering and allied fields. Since its inception in India, department has been continuously engaged in improving its standards in the much sought after OBE philosophy all over the world.
+
+
+
+
+
+						</p>
+						<p>
+							We keep deep faith in providing opportunities to our students to learn and develop themselves beyond the conventional classroom. Many student-centric training workshops, technology oriented industry supported projects, internship programmes provide such opportunities. Several such endeavors imparted knowledge beyond curriculum and added personality development traits among students. They are also encouraged and motivated to compete in several academic and professional competitions resulting in many achievements such as conferences, workshops, seminars and online courses etc. All such efforts enabled students to appear for all campus placements with a professional competence and confidence. This also could generate enormous interest for students of these programmes to appear for competitive examinations such as GATE and public service examinations.
+
+							Significant developments could be witnessed in research and consultancy this year. The departmental highly competent faculty and staff are working out an extra mile to serve the state govt. in getting their important projects audited and approved in time. Many industries are benefitted by expert advice from faculty to enhance their systems and get their audit/approval certificates. The research publications by faculty in international, national journals and conferences show encouraging trend.
+
+							Faculty and staff has been encouraged, and widely supported by TEQIP-III activities, to update and up skill themselves. Many of the faculty has been benefitted by latest technology workshops and FDP through this channel. Several faculties also took NPTEL and other such professional MOOC courses enthusiastically with promising results. Our department also served as partner and mentor to Jabalpur College of Engineering to collaboratively enhance the teaching-learning system and enrich them mutual know how.
+
+
+						</p>
+						<p>
+							
+							This year brought us opportunity to realize our strengths once again in the time of crisis. District of Sangli witnessed disaster of its kind in the event of extraordinary flood that devastated many properties and took many lives. In such a tough time our students and faculty stood by Nobel cause and helped the society. We really feel proud to share those moments when district collector praised our services and bestowed govt. appreciation letter to us. Once again, in the second semester of A.Y.2019-2020 the whole world saw the crisis of pandemic COVID-19; and the department could again have stood by for student’s benefit. With all odds the department was able to complete the online teaching and assessment successfully with in targeted duration by which I am sure, our all students must be having been relieved from great anxiety and pressures.
+
+							I express my sincere thanks to faculty and staff for their sustained efforts and enthusiastic cooperation in building the performance, and achieving significant growth of the department. I am confident that we can certainly meet the challenges of future with the team spirit and support from the motivated people of the department. I thank reader for his/her interest in going through the shared thoughts.
+							<br>
+							<br>
+							Thank you for visiting us.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Mechanical Engineering</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">HOD's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							A very warm welcome to the aspirants for engineering course in Mechanical at Walchand College of Engineering, Sangli. We are moving ahead with a commitment to provide the students with a conductive environment to achieve their career goals.
+
+							Department works for overall growth of students and inculcate the qualities and features that are required and acceptable by Society. Facultymembers and students take initiative for social causes at individual level and as a team under different Banners of various club services of the department.Students from our departments have formed associations known as MESA-MESC, SAE, ISHRAE club etc. to organise different events such as weekly club services, aptitude tests and guidance by eminent personalities.Alumni of departmentachieved top positions and are working with top level companies.
+						</p>
+						<p>
+							The department has a team of highly experienced and motivated faculty members.All the faculty members are well qualified and competitive enough in preparing the young minds i.e. our students for global competition. Our faculty members educate the students by adopting latest teaching and assessment tools. Also, our faculty members organize training programs, guest & expert lectures to aware our students regarding current development and research areas. During the course, students are encouraged to get hands-on experience in the current industrial scenarios through various industrial visits, training, internships, sponsored projects with reputed organisations.
+
+							The website provides an overview of the academic structure, details of activities carried out by department, achievements of students and faculties, industry collaborations, MOUs, profiles of faculty members, placement details, Ph.D. details.
+						</p>
+						<p>
+							We hope upcoming UG, PG and PhDstudents looking for excellent research centre willfind this website to be informative.If you have further queries regarding specialization please do not hesitate to correspond with individual faculty members, or contact them by email, using the faculty pages. Our department looks forward to contribute in innovation and improvisation with active participation from all sections of the society.
+							<br>
+							<br>
+							Thank you for visiting us.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Computer Sciences and Engineering</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">HOD's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							I welcome all the students & their parents to the Department of CSE. Computer Science and Engineering is an essence of today’s world. We are committed to providing not only the technical education to our students but also the leadership qualities through which they can create employment to others.
+
+						</p>
+						<p>
+							The Department of CSE was established in the year 1986. From its inception, it is dedicated towards ensuring great success through quality education for its student. The key factors influencing the quality of education are the quality of faculty, curriculum standards, technological infrastructure available, research environment and strong relation with industries. The CSE department majorly fulfills the performance parameters and also is constantly taking efforts to improve on above mentioned key factors to ensure the success.
+						</p>
+						<p>
+							The goal of department of CSE is to provide innovative and quality education with high standard to achieve academic excellence and provide platform for the students to achieve their career milestones.
+
+							I congratulate to the team of faculty members and the students for their brilliant and genuine efforts and my best wishes to all students and faculty for upcoming academic year.
+							<br>
+							<br>
+							Thank you for visiting us.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Information Technology</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">HOD's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							This message provides an overview of the academic programs, research activities of our department, research facilities, area of interest of faculty members, and details of student activities. We started our journey with the Under-Graduate (UG) programme in 2001. Further, Post Graduate was started in 2012. Since then we have recorded consistent performance in academics and placement. Both UG and PG programmes are NBA accredited.
+						</p>
+						<p>
+							We have grown our expertise and competence in areas like Artificial Intelligence, Machine Learning, Data Science, Open Source Technologies, Web Development, High performance Computing, Internet of Things, etc. The department has 09 state-of-art laboratories for academics and research to meet the changing requirements of industry. Our track based curriculum has been constantly updated by concerning the stakeholders with e-learning (MOOC) platforms for Open Electives, Professional Electives. Department also offers Minor, Honor and Value Added Certification Courses in technological and multidisciplinary environments to provide a wide spectrum to the students for pursuing their interest. We promote our students for higher studies like Masters and Ph. D.
+						</p>
+						<p>
+							We encourage our students to participate in various extracurricular activities through student associations like SAIT, WLUG, etc. The Department seeks to combine excellence in education with service to the employers, researchers and students.
+							<br>
+							<br>
+							Thank you for visiting us.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
